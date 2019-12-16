e4008b1f9923e31b88af11785cec3aac240f1e63
micekrRC <rmicek@rscsolutions.com>
2019-12-16 15:14:45 -0500

    add details (id, description) to page, and some logic to load the data on page load

--- frontend-spring-boot-vue-crud-full-stack/src/components/CourseComponent.vue
+++ frontend-spring-boot-vue-crud-full-stack/src/components/CourseComponent.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    <h1>Course Details</h1>
+      <h3>Course</h3>
+      <div>{{id}}</div>
+      <div>{{description}}</div>
   </div>
 </template>
 
 <script>
+import CourseDataService from '../service/CourseDataService';
 export default {
   name: "courseDetails",
+  data() {
+    return {
+      description: "",
+      INSTRUCTOR: "in28minutes",
+      errors: []
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.params.id;
+    }
+  },
+  methods: {
+    refreshCourseDetails() {
+        CourseDataService.retrieveCourse(this.INSTRUCTOR, this.id).then(res => {
+          this.description = res.data.description;
+        });
+    },
+  },
+  created() {
+    this.refreshCourseDetails();
+  }
 };
 </script>
 
