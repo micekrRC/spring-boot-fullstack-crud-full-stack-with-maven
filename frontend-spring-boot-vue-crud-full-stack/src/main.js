c57909174592d0288b672d428827c11bd26b2158
micekrRC <rmicek@rscsolutions.com>
2019-12-16 13:05:02 -0500

    add router

--- frontend-spring-boot-vue-crud-full-stack/src/main.js
+++ frontend-spring-boot-vue-crud-full-stack/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// add routes
+import router from "./routes";
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,    // for routing
   render: h => h(App),
 }).$mount('#app')
