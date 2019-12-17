6bb571804210bf808ac277355cf274a896ed2350
micekrRC <rmicek@rscsolutions.com>
2019-12-17 13:33:50 -0500

    added update and create methods

--- frontend-spring-boot-vue-crud-full-stack/src/service/CourseDataService.js
+++ frontend-spring-boot-vue-crud-full-stack/src/service/CourseDataService.js
@@ -8,11 +8,35 @@ forming the URL to call in a reusable way
 const INSTRUCTOR_API_URL = `${COURSE_API_URL}/instructors/${INSTRUCTOR}`;
 
 class CourseDataService {
-  retrieveAllCourses() {
 
-    // Call the REST API with the GET method
-    return axios.get(`${INSTRUCTOR_API_URL}/courses`);
-  }
+    retrieveCourse(name, id) {
+        return axios.get(`${INSTRUCTOR_API_URL}/courses/${id}`);
+    }
+
+    retrieveAllCourses() {
+
+        // Call the REST API with the GET method
+        return axios.get(`${INSTRUCTOR_API_URL}/courses`);
+    }
+    /*
+    Enhancing Vue app with Delete Course Feature
+    Let’s add deleteCourse method to CourseDataService. 
+    As you can see it execute the delete request to specific course api url.
+    */
+    deleteCourse(name, id) {
+        return axios.delete(`${INSTRUCTOR_API_URL}/courses/${id}`);
+    }
+
+    updateCourse(name, id, course) {
+        return axios.put(`${INSTRUCTOR_API_URL}/courses/${id}`, course);
+    }
+  
+    createCourse(name, course) {
+        return axios.post(`${INSTRUCTOR_API_URL}/courses/`, course);
+    }
+
+
+
 }
 // We are creating an instance of CourseDataService and making it available for other components.
 export default new CourseDataService();
